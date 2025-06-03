e29d69fe87a8ef4e70ca3408f45ace4643c61519
AviAsnani <aviasnani@MACBOOKAIR-3E78.station>
2025-06-03 19:45:51 +0100

    pulled origin

---
+++ backend/models/friend_request.py
@@ -0,0 +1,10 @@
+from extenions import db
+from datetime import datetime, timezone
+
+class FriendRequest(db.Model):
+  id = db.Column(db.Integer, primary_key=True, nullable=False)
+  from_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  to_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  joined_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+
+
--- frontend/src/firebase.config.js
+++ frontend/src/firebase.config.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-
+is
 const app = initializeApp(firebaseConfig);
 
 // Firebase Authentication setup
